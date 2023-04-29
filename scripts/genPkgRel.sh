5af9d45d43f6003753a8c8f29d9a80f65fa61ab1
Filip Rojek <filip@filiprojek.cz>
2023-04-29 11:10:21 +0200

    feat: multiple archs - TEST

--- scripts/genPkgRel.sh
+++ scripts/genPkgRel.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 DIR="$(cd "$(dirname "$0")" && pwd)"
+LS="$(which ls)"
+ARCHS="$($LS -l apt-repo/dists/stable/main/ | awk '{print $9}' | awk NF | awk -F - '{print $2}')"
 
 # generate Packages file
-dpkg-scanpackages --multiversion --arch amd64 "./apt-repo/pool/" > "./apt-repo/dists/stable/main/binary-amd64/Packages"
-# compress Packages file
-cat "./apt-repo/dists/stable/main/binary-amd64/Packages" | gzip -9 > "./apt-repo/dists/stable/main/binary-amd64/Packages.gz"
+for ARCH in $ARCHS; do
+	echo $ARCH
+	dpkg-scanpackages --multiversion --arch $ARCH "./apt-repo/pool/" > "./apt-repo/dists/stable/main/binary-$ARCH/Packages"
+	# compress Packages file
+	cat "./apt-repo/dists/stable/main/binary-$ARCH/Packages" | gzip -9 > "./apt-repo/dists/stable/main/binary-$ARCH/Packages.gz"
+done
+
 # generate Release file
 cd "./apt-repo/dists/stable/"
 $DIR/generate-release.sh > "Release"
