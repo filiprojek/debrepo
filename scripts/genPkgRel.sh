56492964867e512c6d5b279960bd93f40af2e411
Filip Rojek <filip@filiprojek.cz>
2023-05-19 19:53:27 +0200

    fix: wrong path

--- scripts/genPkgRel.sh
+++ scripts/genPkgRel.sh
@@ -5,14 +5,14 @@ REPODIR="$1"
 ARCHS="$($LS -l $REPODIR/apt-repo/dists/stable/main/ | awk '{print $9}' | awk NF | awk -F - '{print $2}')"
 
 # generate Packages file
+cd $REPODIR/apt-repo/
 for ARCH in $ARCHS; do
-	cd $REPODIR/apt-repo/
 	dpkg-scanpackages --multiversion --arch $ARCH "pool/" > "dists/stable/main/binary-$ARCH/Packages"
 	# compress Packages file
 	cat "dists/stable/main/binary-$ARCH/Packages" | gzip -9 > "dists/stable/main/binary-$ARCH/Packages.gz"
 done
 
 # generate Release file
-cd "$REPODIR/apt-repo/dists/stable/"
+cd "dists/stable/"
 $DIR/generate-release.sh > "Release"
 
